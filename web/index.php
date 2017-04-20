32b2c029d3ee5b9182da480f92bf6fe94679e028
Ju/TroisYaourts <julien@troisyaourts.com>
2017-04-20 09:55:01 +0200

    install libs

---
+++ web/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Ai</title>
+  <script src="/bower_components/angular/angular.js"></script>
+  <script src="/bower_components/angular-foundation-6/dist/angular-foundation.min.js"></script>
+  <script src="/bower_components/angular-touch/angular-touch.min.js"></script>
+
+  <link rel="stylesheet" href="/bower_components/foundation-sites/dist/css/foundation-flex.min.css" />
+</head>
+<body>
+  
+</body>
+</html>
